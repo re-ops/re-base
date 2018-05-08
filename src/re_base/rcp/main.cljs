03aa3b0867a04fe5fb22c8c3caa1685bc63b35f0
ronen <narkisr@gmail.com>
2018-05-09 00:04:27 +1000

    version assertion and default env

--- src/re_base/rcp/main.cljs
+++ src/re_base/rcp/main.cljs
@@ -3,18 +3,17 @@
    [cljs.core.async :as async :refer [take!]]
    [re-base.rcp.shell]
    [re-conf.cljs.pkg :as p :refer (initialize)]
-   [re-conf.cljs.core :refer (invoke)]
-   [re-conf.cljs.log :refer (info debug error)]
-   ))
+   [re-conf.cljs.core :refer (invoke assert-node-major-version)]
+   [re-conf.cljs.log :refer (info debug error)]))
 
 (defn -main [& args]
   (assert-node-major-version)
   (take! (initialize)
          (fn [r]
            (info "Started re-conf provisioning re-base" ::main)
-           (invoke re-base.rcp.shell {}))))
+           (invoke re-base.rcp.shell {:user "re-ops"}))))
 
 (set! *main-cli-fn* -main)
 
-
-
+(comment
+  (-main))
