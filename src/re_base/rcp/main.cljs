06ef48f94661af4bb95c6217764e05e34d022011
ronen <narkisr@gmail.com>
2018-05-16 15:09:30 +0200

    adding env slurp support

--- src/re_base/rcp/main.cljs
+++ src/re_base/rcp/main.cljs
@@ -1,19 +1,21 @@
 (ns re-base.rcp.main
   (:require
    [cljs.core.async :as async :refer [take!]]
+   [cljs-node-io.core :as io]
    [re-base.rcp.shell]
    [re-conf.cljs.pkg :as p :refer (initialize)]
    [re-conf.cljs.core :refer (invoke assert-node-major-version)]
    [re-conf.cljs.log :refer (info debug error)]))
 
-(defn -main [& args]
+(defn -main [e & args]
   (assert-node-major-version)
-  (take! (initialize)
+  (let [env (if e (cljs.reader/read-string (io/slurp f)) {})]
+    (take! (initialize)
          (fn [r]
-           (info "Started re-conf provisioning re-base" ::main)
-           (invoke re-base.rcp.shell {:user "re-ops"}))))
+           (info "Started provisioning using re-base" ::main)
+           (invoke re-base.rcp.shell env)))))
 
 (set! *main-cli-fn* -main)
 
 (comment
-  (-main))
+  (-main "resources/dev.edn"))
