0a2986daeb3a151a8deef953950eb8e54472280c
ronen <narkisr@gmail.com>
2018-05-17 17:10:21 +0200

    Adding zsh and using home

--- src/re_base/rcp/shell.cljs
+++ src/re_base/rcp/shell.cljs
@@ -5,21 +5,36 @@
   (:require
    [re-conf.cljs.log :refer (info debug error)]
    [re-conf.cljs.pkg :refer (package)]
-   [re-conf.cljs.file :refer (chown directory symlink)]
-   [re-conf.cljs.facts :refer (home)]
+   [re-conf.cljs.shell :refer (unless)]
+   [re-conf.cljs.file :refer (chown directory symlink contains)]
    [re-conf.cljs.git :refer (clone)]
    [re-conf.cljs.shell :refer (exec)]
    [re-conf.cljs.output :refer (summary)]))
 
 (defn tmux
   "Setup tmux for user"
-  [{:keys [user gid uid]}]
-  (let [home (<< "/home/~{user}") dest (<< "~{home}/.tmux")]
+  [{:keys [home gid uid]}]
+  (let [dest (<< "~{home}/.tmux")]
     (->
      (package "tmux")
      (clone "git://github.com/narkisr/.tmux.git" dest)
      (directory (<< "~{dest}/plugins/") :present)
      (chown dest uid gid)
      (clone "git://github.com/tmux-plugins/tpm" (<< "~{dest}/plugins/tpm"))
-     (symlink  (<< "~{home}/.tmux.conf") (<< "{dest}/.tmux.conf") :present))))
+     (symlink (<< "~{dest}/.tmux.conf") (<< "~{home}/.tmux.conf") :present))))
 
+(defn zsh
+  "zsh setup"
+  [{:keys [home user]}]
+  (let [dest (<< "~{home}/.tmux")]
+    (->
+     (package "zsh")
+     (contains "/etc/passwd" "re-ops:/bin/zsh")
+     (unless "/usr/bin/chsh" "-s" "/usr/bin/zsh" user))))
+
+(defn oh-my-zsh
+  "Setup https://github.com/robbyrussell/oh-my-zsh"
+  [{:keys [home uid gid]}]
+  (let [dest (<< "~{home}/.oh-my-zsh")]
+    (clone "git://github.com/narkisr/oh-my-zsh.git" dest)
+    (symlink  (<< "~{home}/.zshrc") (<< "{dest}/.zshrc") :present)))
