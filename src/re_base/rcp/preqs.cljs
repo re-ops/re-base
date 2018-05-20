17931106b75d872ef85d0500d3e94276fa6147a3
re-ops <narkisr@gmail.com>
2018-05-20 16:35:03 +0200

    bbq prequisits

--- src/re_base/rcp/preqs.cljs
+++ src/re_base/rcp/preqs.cljs
@@ -1,9 +1,10 @@
 (ns re-base.rcp.preqs
   "Resource prerequisites"
+  (:refer-clojure :exclude [update key remove])
   (:require-macros
    [clojure.core.strint :refer (<<)])
   (:require
-   [re-conf.resources.pkg :refer (package)]
+   [re-conf.resources.pkg :refer (package repository key update)]
    [re-conf.resources.file :refer (template)]
    [re-conf.resources.output :refer (summary)]))
 
@@ -11,8 +12,16 @@
   "git setup"
   [{:keys [home git]}]
   (let [dest (<< "~{home}/.gitconfig")]
-    (println dest)
     (->
      (package "git")
      (template git "resources/gitconfig.mustache" dest)
      (summary "git setup"))))
+
+(defn barbecue
+  "Setting up barbecue repo"
+  []
+  (->
+   (repository "https://raw.githubusercontent.com/narkisr/fpm-barbecue/repo/packages/ubuntu/" :present)
+   (key "keyserver.ubuntu.com" "42ED3C30B8C9F76BC85AC1EC8B095396E29035F0")
+   (update)
+   (summary "barbecue setup")))
