331554c2447542187a6e5c293fee6f3e8af1a504
re-ops <narkisr@gmail.com>
2018-05-20 05:38:19 +0200

    Adding octo

--- src/re_base/rcp/backup.cljs
+++ src/re_base/rcp/backup.cljs
@@ -1,7 +1,9 @@
 (ns re-base.rcp.backup
   "Backup utilities recipes"
+  (:refer-clojure :exclude [update key remove])
   (:require
    [re-conf.resources.download :refer (download checksum)]
+   [re-conf.resources.pkg :refer (package repository key update)]
    [re-conf.resources.archive :refer (bzip2)]
    [re-conf.resources.shell :refer (exec)]
    [re-conf.resources.output :refer (summary)]))
@@ -10,10 +12,21 @@
   "Setting up restic"
   []
   (let [dest "/tmp/restic_0.8.3_linux_amd64.bz2"
-        sha "1e9aca80c4f4e263c72a83d4333a9dac0e24b24e1fe11a8dc1d9b38d77883705"
+        expected "1e9aca80c4f4e263c72a83d4333a9dac0e24b24e1fe11a8dc1d9b38d77883705"
         url "https://github.com/restic/restic/releases/download/v0.8.3/restic_0.8.3_linux_amd64.bz2"]
     (->
      (download url dest)
-     (checksum dest sha :sha256)
+     (checksum dest expected :sha256)
      (bzip2 dest)
-     (summary "installing restic done"))))
+     (summary "restic setup"))))
+
+(defn octo
+  "Setting up Octo"
+  []
+  (->
+   (package "openjdk-8-jre")
+   (repository "https://raw.githubusercontent.com/narkisr/fpm-barbecue/repo/packages/ubuntu/" :present)
+   (key "keyserver.ubuntu.com" "42ED3C30B8C9F76BC85AC1EC8B095396E29035F0")
+   (update)
+   (package "octo" :present)
+   (summary "octo setup")))
