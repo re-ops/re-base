de1629c5446e6c97c70025b6ed7df6eaa42e7597
re-ops <narkisr@gmail.com>
2018-05-21 16:13:14 +0200

    lazy download use and reversing headless dropbox on servers

--- src/re_base/rcp/backup.cljs
+++ src/re_base/rcp/backup.cljs
@@ -5,7 +5,7 @@
   (:refer-clojure :exclude [update key remove])
   (:require
    [re-conf.resources.pkg :refer (package repository key update)]
-   [re-conf.resources.download :refer (download checksum)]
+   [re-conf.resources.download :refer (download)]
    [re-conf.resources.pkg :refer (package)]
    [re-conf.resources.facts :refer (desktop?)]
    [re-conf.resources.file :refer (directory)]
@@ -20,8 +20,7 @@
         expected "1e9aca80c4f4e263c72a83d4333a9dac0e24b24e1fe11a8dc1d9b38d77883705"
         url "https://github.com/restic/restic/releases/download/v0.8.3/restic_0.8.3_linux_amd64.bz2"]
     (->
-     (download url dest)
-     (checksum dest expected :sha256)
+     (download url dest expected :sha256)
      (bzip2 dest)
      (summary "restic setup"))))
 
@@ -49,23 +48,26 @@
 
 (defn dropbox-headless
   "Setting up headless dropbox"
-  []
+  [{:keys [home]}]
   (let [archive "/tmp/dropbox.tar.gz"
         url "https://www.dropbox.com/download?plat=lnx.x86_64"
+        py_url "https://www.dropbox.com/download?dl=packages/dropbox.py"
+        py_bin (<< "~{home}/bin/dropbox.py")
         dest "/usr/local/dropbox-deamon"]
     (if (desktop?)
       (->
-        (download url archive)
-        (untar archive "/tmp/")
-        (directory dest :present)
-        (exec "test" "-d" (<< "~{dest}/.dropbox-dist"))
-        (unless "/bin/mv" "/tmp/.dropbox-dist" dest)
-        (summary "headless dropbox setup"))
+       (repository "deb http://linux.dropbox.com/ubuntu xenial main" :present)
+       (key "pgp.mit.edu" "1C61A2656FB57B7E4DE0F4C1FC918B335044912E")
+       (update)
+       (package "dropbox")
+       (summary "dropbox setup"))
       (->
-        (repository "deb http://linux.dropbox.com/ubuntu xenial main" :present)
-        (key "pgp.mit.edu" "1C61A2656FB57B7E4DE0F4C1FC918B335044912E")
-        (update)
-        (package "dropbox")
-        (summary "dropbox setup"))
-      )))
+       (download url archive)
+       (untar archive "/tmp/")
+       (directory dest :present)
+       (exec "test" "-d" (<< "~{dest}/.dropbox-dist"))
+       (unless "/bin/mv" "/tmp/.dropbox-dist" dest)
+       (directory (<< "~{home}/bin") :present)
+       (download py_url py_bin "d7e01a4d178674f1895dc3f74adb7f36" :md5)
+       (summary "headless dropbox setup")))))
 
