a054094e7dbc0742a72a91ecc9ba73a33f662df0
re-ops <narkisr@gmail.com>
2018-05-26 17:50:38 +0200

    Adding desktop

---
+++ src/re_base/rcp/desktop.cljs
@@ -0,0 +1,27 @@
+(ns re-base.rcp.desktop
+  "Desktop setup"
+  (:require-macros
+   [clojure.core.strint :refer (<<)])
+  (:refer-clojure :exclude [update])
+  (:require
+   [re-conf.resources.download :refer (download)]
+   [re-conf.resources.pkg :refer (install)]
+   [re-conf.resources.output :refer (summary)])
+ )
+
+(defn chrome
+   "Google chrome setup"
+   []
+   (let [repo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
+         key "https://dl-ssl.google.com/linux/linux_signing_key.pub"
+         dest "/tmp/chrome.key"]
+     (-> 
+       (install repo key "google-chrome-stable")
+       (summary "google-chrome install"))))
+
+
+(defn xmonad
+   "Setting up Xmonad"
+   []
+
+  )
