f4a1e76dfbbeaa601b98c159ea8fea17f1925d2c
ronen <narkisr@gmail.com>
2018-05-28 01:06:55 +1000

    serializing preqs and rest ns

--- src/re_base/rcp/main.cljs
+++ src/re_base/rcp/main.cljs
@@ -5,8 +5,10 @@
    [cljs.core.async :as async :refer [take!]]
    [cljs-node-io.core :as io]
    [re-base.rcp.shell]
-   [re-base.rcp.preqs]
+   [re-base.rcp.docker]
+   [re-base.rcp.desktop]
    [re-base.rcp.backup]
+   [re-base.rcp.preqs]
    [re-conf.resources.pkg :as p :refer (initialize)]
    [re-conf.core :refer (invoke assert-node-major-version)]
    [re-conf.resources.log :refer (info debug error)]))
@@ -16,10 +18,13 @@
   (let [env (if e (cljs.reader/read-string (io/slurp e)) {})]
     (take! (initialize)
            (fn [r]
-             (info "Started provisioning using re-base" ::main)
-             (invoke re-base.rcp.preq env)
-             (invoke re-base.rcp.backup env)
-             (invoke re-base.rcp.shell env)))))
+             (info "Started invokeing using re-base" ::main)
+             (take! (invoke re-base.rcp.preqs env)
+                    (fn [r]
+                      (invoke re-base.rcp.backup env)
+                      (invoke re-base.rcp.docker env)
+                      (invoke re-base.rcp.desktop env)
+                      (invoke re-base.rcp.shell env)))))))
 
 (set! *main-cli-fn* -main)
 
