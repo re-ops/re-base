059d68067bca3ac251dc64550f1fef9f253d612b
ronen <narkisr@gmail.com>
2018-07-04 22:09:10 +1000

    fixing ownership and permissions

--- src/re_base/rcp/reops.cljs
+++ src/re_base/rcp/reops.cljs
@@ -4,17 +4,23 @@
    [clojure.core.strint :refer (<<)])
   (:require
    [re-conf.resources.pkg :refer (package)]
-   [re-conf.resources.file :refer (copy chmod template)]
+   [re-conf.resources.file :refer (copy chmod template chown)]
    [re-conf.resources.user :refer (user)]
    [re-conf.resources.output :refer (summary)]))
 
 (defn reops-user
   "Setting up reops user"
-  [{:keys [ssh]}]
-  (->
-   (user "re-ops" :present {:home true})
-   (template ssh "resources/ssh/authorized_keys.mustache" "/home/re-ops/.ssh/authorized_keys")
-   (summary "re-ops user setup")))
+  [{:keys [ssh users]}]
+  (let [{:keys [gid uid name]} (:re-ops users)
+        home (<< "/home/~{name}")]
+    (->
+     (user "re-ops" :present {:home true :uid uid :gid gid})
+     (template ssh "resources/ssh/authorized_keys.mustache" (<< "~{home}/.ssh/authorized_keys"))
+     (chmod (<< "~{home}/.ssh/authorized_keys") 600)
+     (chmod (<< "~{home}/.ssh/") 700)
+     (chown (<< "~{home}/.ssh/") uid gid)
+     (chown (<< "~{home}/.ssh/authorized_keys") uid gid)
+     (summary "re-ops user done"))))
 
 (defn reops-scripts
   "Setting up re-ops scripts"
@@ -25,7 +31,7 @@
      (chmod (<< "~{prefix}/apt-cleanup") 755)
      (copy "resources/reops/purge-kernels" (<< "~{prefix}/purge-kernels"))
      (chmod (<< "~{prefix}/purge-kernels") 755)
-     (summary "re-ops scripts"))))
+     (summary "re-ops scripts done"))))
 
 (defn re-gent
   "Re-gent support"
@@ -33,4 +39,4 @@
   (->
    (package "openjdk-8-jre-headless" :present)
    (package "sysstat" :present)
-   (summary "re-ops scripts")))
+   (summary "re-gent setup done")))
