dafb1b9943f2cd4a51c4df63633f8f70284285fa
ronen <narkisr.com>
2018-10-17 20:19:28 +1100

    inotify

--- src/re_base/recipes/baseline.cljs
+++ src/re_base/recipes/baseline.cljs
@@ -3,6 +3,8 @@
   (:require-macros
    [clojure.core.strint :refer (<<)])
   (:require
+   [re-conf.resources.file :refer (line)]
+   [re-conf.resources.shell :refer (exec)]
    [re-conf.resources.output :refer (summary)]
    [re-conf.resources.pkg :refer (package)]
    [re-conf.resources.download :refer (download)]))
@@ -17,5 +19,13 @@
 (defn ssh-tools []
   (->
    (package "sshfs")
-   (package "mosh"))
-  (summary "ssh tools done"))
+   (package "mosh")
+   (summary "ssh tools done")))
+
+(defn inotify-max
+  "Increasing max inotify watches"
+  []
+  (->
+   (line "/etc/sysctl.conf" "fs.inotify.max_user_watches = 1048576")
+   (exec "/sbin/sysctl" "-p")
+   (summary "inotify-max done")))
